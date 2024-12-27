304b7ac5b16a8c951ae8c14d4888e94594a30ef3
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-27 14:15:38 +0000

    word editing

--- src/Components/TopicsList.jsx
+++ src/Components/TopicsList.jsx
@@ -1,36 +1,37 @@
-import { useState, useEffect} from "react"
-import { getTopics } from "../api"
-import SelectedTopic from "./SelectedTopic"
+import { useState, useEffect } from "react";
+import { getTopics } from "../api";
+import SelectedTopic from "./SelectedTopic";
 
 const TopicsList = () => {
-    const [topics, setTopics] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+  const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        getTopics().then((topics) => {
-            setTopics(topics)
-            setIsLoading(false)
-        }).catch((error) => {
-            setIsLoading(false)
-            return error
-        })
-    }, [])
+  useEffect(() => {
+    getTopics()
+      .then((topics) => {
+        setTopics(topics);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        return error;
+      });
+  }, []);
 
-    if (isLoading) {
-        return <p className="loading">Loading Topics</p>
-    }
+  if (isLoading) {
+    return <p className="loading">Loading Topics</p>;
+  }
 
-    return (
-        <>
-        <h2>View articles by topic: </h2>
-        <ul>
-            {topics.map((topic) => {
-                return <SelectedTopic topic={topic} key={topic.slug} />
-            })}
-        </ul>
-        </>
-    )
+  return (
+    <>
+      <h2>View Articles by Topic: </h2>
+      <ul>
+        {topics.map((topic) => {
+          return <SelectedTopic topic={topic} key={topic.slug} />;
+        })}
+      </ul>
+    </>
+  );
+};
 
-}
-
-export default TopicsList
+export default TopicsList;
